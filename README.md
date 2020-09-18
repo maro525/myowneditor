d7588fce09a04e24c5cdc8198cfe6bb869a5e0d0
Hidemaro Fujinami <hdmrf.fjnm@gmail.com>
2020-09-18 17:01:05 +0900

    file io

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # My Own Editor In Rust
+
+## ref
+
+https://qiita.com/hatoo@github/items/905a19a98876e7446edf
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,67 @@
+extern crate clap;
 extern crate termion;
 
+use clap::{App, Arg};
+use std::ffi::OsStr;
+use std::fs;
 use std::io::{stdin, stdout, Write};
+use std::path;
+use termion::clear;
+use termion::cursor;
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
 
 fn main() {
-    let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
+    // clap
+    let matches = App::new("myowneditor")
+        .about("A text editor")
+        .bin_name("myowneditor")
+        .arg(Arg::with_name("file"))
+        .get_matches();
 
+    // file
+    let file_path: Option<&OsStr> = matches.value_of_os("file");
+
+    // read text
+    let buffer: Vec<Vec<char>> = file_path
+        .and_then(|file_path| {
+            fs::read_to_string(path::Path::new(file_path))
+                .ok()
+                .map(|s| {
+                    s.lines()
+                        .map(|line| line.trim_right().chars().collect())
+                        .collect()
+                })
+        })
+        .unwrap_or(Vec::new());
+
+    // std
+    let stdin = stdin();
+
+    let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
+
+    // clear screen
+    write!(stdout, "{}", clear::All);
+    // cursor
+    write!(stdout, "{}", cursor::Goto(1, 1));
+
+    // test pring
+    // write!(stdout, "{}", "Hello World!");
+    
+    for line in &buffer {
+        for &c in line {
+            write!(stdout, "{}", c);
+        }
+
+        write!(stdout, "\r\n");
+    }
+
+    // flash
+    stdout.flush().unwrap();
+
+    // events
     for evt in stdin.events() {
         if evt.unwrap() == Event::Key(Key::Ctrl('c')) {
             return;
