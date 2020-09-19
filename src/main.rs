8d1ec36f4af6860202fec21c7e0ed4a894ba4b24
maro525 <hdmrf.fjnm@gmail.com>
2020-09-19 22:35:45 +0900

    cursor

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate clap;
 extern crate termion;
 
 use clap::{App, Arg};
+use std::cmp::min;
 use std::ffi::OsStr;
 use std::fs;
 use std::io::{stdin, stdout, Write};
@@ -12,6 +13,12 @@ use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Cursor {
+    row: usize,
+    column: usize
+}
 
 fn main() {
     // clap
@@ -36,6 +43,9 @@ fn main() {
                 })
         })
         .unwrap_or(Vec::new());
+
+    // cursor
+    let mut cursor = Cursor { row: 0, column: 0 };
 
     // std
     let stdin = stdin();
@@ -63,8 +73,40 @@ fn main() {
 
     // events
     for evt in stdin.events() {
-        if evt.unwrap() == Event::Key(Key::Ctrl('c')) {
-            return;
+        match evt.unwrap() {
+            Event::Key(Key::Ctrl('c')) => {
+                return;
+            }
+            
+            Event::Key(Key::Up) => {
+                if cursor.row > 0 {
+                    cursor.row -= 1;
+                    cursor.column = min(buffer[cursor.row].len(), cursor.column);
+                }
+            }
+            Event::Key(Key::Down) => {
+                if cursor.row + 1 < buffer.len() {
+                    cursor.row += 1;
+                    cursor.column = min(cursor.column, buffer[cursor.row].len());
+                }
+            }
+            Event::Key(Key::Left) =>  {
+                if cursor.column > 0 {
+                    cursor.column -= 1;
+                }
+            }
+            Event::Key(Key::Right) => {
+                cursor.column = min(cursor.column + 1, buffer[cursor.row].len());
+            }
+            _ => {}
         }
+
+        write!(
+            stdout,
+            "{}",
+            cursor::Goto(cursor.column as u16 + 1, cursor.row as u16 + 1)
+        );
+
+        stdout.flush().unwrap();
     }
 }
